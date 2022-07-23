137a1e7d655bd17ea647a85aeda61be873fbbe1c
Edward Arias <jayson@admins-MacBook-Pro.local>
2022-07-23 18:54:45 -0400

    with streamlit

--- time_left.py
+++ time_left.py
@@ -1,15 +1,35 @@
-# 🚨 Don't change the code below 👇
-age = input("What is your current age?\n")
-# 🚨 Don't change the code above 👆
+import streamlit as st
 
-#Write your code below this line 👇
+st.title('Time left to enjoy your time on this earth.')
 
+age = st.number_input("What is your current age?\n", 0, 100)
 
-years = 90 - int(age)
+years = 105 - age
 
 days = round(years * 365)
 weeks = round(years * 52)
 months = round(years * 12)
 
+st.markdown(f"You have {days} days, {weeks} weeks, and {months} months left. Live in the moment!")
 
-print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
+
+
+
+
+# days = st.write(years * 365)
+# weeks = st.write(years * 52)
+# months = st.write(years * 12)
+
+# st.write(print(f"You have {days} days, {weeks} weeks, and {months} months left."))
+
+
+
+
+# age = input("What is your current age?\n")
+# years = 90 - int(age)
+# days = round(years * 365)
+# weeks = round(years * 52)
+# months = round(years * 12)
+# print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+
